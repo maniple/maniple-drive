f8710541d2f0240bdb8c4551b912c48c5b8f7848
xemlock <xemlock@gmail.com>
2014-07-29 05:24:54 +0200

    updated dir types

--- module/library/DirType.php
+++ module/library/DirType.php
@@ -1,11 +1,16 @@
 <?php
 
+/**
+ * Predefined directory types.
+ */
 abstract class ManipleDrive_DirType
 {
     const TYPE_DOCUMENTS = 'documents';
     const TYPE_GAMES     = 'games';
     const TYPE_IMAGES    = 'images';
     const TYPE_MUSIC     = 'music';
+    const TYPE_PHOTOS    = 'photos';
     const TYPE_PICTURES  = 'pictures';
+    const TYPE_SLIDES    = 'slides';
     const TYPE_VIDEOS    = 'videos';
 }
