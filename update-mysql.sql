6cc1232d0f3de2f75febe557419d037cc8cde43a
xemlock <xemlock@gmail.com>
2014-11-10 18:42:43 +0100

    mysql schema update

--- update-mysql.sql
+++ update-mysql.sql
@@ -5,3 +5,9 @@ ALTER TABLE {PREFIX}drive_dirs ADD COLUMN is_hidden TINYINT(1) NOT NULL DEFAULT 
 ALTER TABLE {PREFIX}drive_dirs ADD COLUMN system_count INTEGER UNSIGNED NOT NULL DEFAULT 0 AFTER is_hidden;
 
 ALTER TABLE {PREFIX}drive_dirs ADD COLUMN handler VARCHAR(32);
+
+ALTER TABLE {PREFIX}drive_dirs DROP FOREIGN KEY drive_dirs_drive_id_fkey;
+ALTER TABLE {PREFIX}drive_dirs DROP FOREIGN KEY drive_dirs_drive_id_parent_id_fkey;
+ALTER TABLE {PREFIX}drive_dirs DROP INDEX drive_dirs_drive_id_dir_id_idx;
+ 
+ALTER TABLE {PREFIX}drive_dirs MODIFY COLUMN drive_id INTEGER NULL;
